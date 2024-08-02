a4b0e4c193d1796fa29797383960d47d3a7dbc21
Ivar-7 <leank6071@gmail.com>
2024-08-02 13:37:07 +0300

    add env variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
 import 'package:chat_app/screens/splash.dart';
 import 'package:chat_app/screens/chat.dart';
 import 'package:chat_app/screens/auth.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load();
   runApp(const App());
 }
 
