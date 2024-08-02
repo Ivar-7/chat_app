dc38d34fadad712557fc3247286d70d4a196637d
Ivar-7 <leank6071@gmail.com>
2024-08-02 09:26:01 +0300

    add new messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:chat_app/screens/splash.dart';
 import 'package:chat_app/screens/chat.dart';
-import 'firebase_options.dart';
 import 'package:chat_app/screens/auth.dart';
 
 void main() async {
